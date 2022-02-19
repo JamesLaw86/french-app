6df1572abd151037da2bd0efc65c3be12b0278a8
James Law <jameslawbw@gmail.com>
2022-02-19 17:12:39 +0000

    Initial commit

---
+++ FrenchApp/FrenchApp/FrenchApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FrenchApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FrenchApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
